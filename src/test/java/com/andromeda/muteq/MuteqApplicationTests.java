bdba4f61fa5979c64a4486a117da59fd2312e2ea
Edison Furusato <edison.oda@hotmail.com>
2025-02-05 20:01:46 -0300

    Initial commit

---
+++ src/test/java/com/andromeda/muteq/MuteqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andromeda.muteq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuteqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
